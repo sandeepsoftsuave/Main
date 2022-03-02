6e3270d4848d2a20cfe14a7f07257ea381b816be
sandeep <sandeep.samal@softsuave.com>
2022-03-02 17:42:37 +0530

    - added div in app drawer of sub module

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
-
+import AppDrawer from './SubTest/src/AppDrawer'
 function App() {
   return (
     <div className="App">
      <div>
        <h1>Main Test</h1>
+       <AppDrawer />
        </div>
     </div>
   );
